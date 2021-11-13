dbe0808c551d4b3d6adfff1bd58a542a7ceb039b
iosio <iosio.design@gmail.com>
2021-11-13 11:58:24 -0800

    .

--- src/allUtils.js
+++ src/allUtils.js
@@ -1,11 +1,4 @@
-import toPath from 'lodash/toPath'
-import clone from 'lodash/clone'
-
-/*
-    I may be continuing to alias my old stuff with lodash-es where applicable.
-    ... correction... "lodash" not "lodash-es" cuz its not playing well with the bundlers
- */
-
+//
 /*################################
 ##################################
 
@@ -457,6 +450,9 @@ export const assign = (obj, props) => {
 export const extend = assign;
 
 const emptyTarget = val => isObj(val) ? {} : isArray(val) ? [] : val;
+
+export const shallowCopy = objOrArr => assign(emptyTarget(objOrArr), objOrArr)
+
 export const deepCopy = o => {
     let copy = emptyTarget(o);
     if (isObjOrArr(o)) for (let k in o) copy[k] = isObjOrArr(o[k]) ? deepCopy(o[k]) : o[k];
@@ -485,98 +481,45 @@ export const deepMergeObj = (target, source, options = {clone: true}) => {
     return output;
 };
 
+// https://youmightnotneed.com/lodash/
+// toPath('a[0].b.c') // => ['a', '0', 'b', 'c']
+export const toPath = (path) => path.match(/([^[.\]])+/g);
 
 export function getIn(obj, key, def, p = 0) {
-    const path = toPath(key);
+    const path = isArray(key) ? key : toPath(key);
     while (obj && p < path.length) obj = obj[path[p++]];
     return obj === undefined ? def : obj;
 }
 
-//https://github.com/developit/dlv
-export const dlv = (obj, key, def, p, undef) => {
-    key = key.split ? key.split('.') : key;
-    for (p = 0; p < key.length; p++) {
-        obj = obj ? obj[key[p]] : undef;
-    }
-    return obj === undef ? def : obj;
-};
-
-// https://github.com/kalmbach/bury/blob/master/index.js
-export const bury = (obj, key, value) => {
-    key = key.split ? key.split('.') : key;
-
-    let p = 0;
-    let z = key[key.length - 1];
-
-    while (obj.hasOwnProperty(key[p]) && key[p] !== z) obj = obj[key[p++]];
-    return obj.hasOwnProperty(z) ? obj[z] = value : undefined;
-}
-
-// https://github.com/formium/formik/blob/master/packages/formik/src/utils.ts
-/**
- * Deeply set a value from in object via it's path. If the value at `path`
- * has changed, return a shallow copy of obj with `value` set at `path`.
- * If `value` has not changed, return the original `obj`.
- *
- * Existing objects / arrays along `path` are also shallow copied. Sibling
- * objects along path retain the same internal js reference. Since new
- * objects / arrays are only created along `path`, we can test if anything
- * changed in a nested structure by comparing the object's reference in
- * the old and new object, similar to how russian doll cache invalidation
- * works.
- *
- * In earlier versions of this function, which used cloneDeep, there were
- * issues whereby settings a nested value would mutate the parent
- * instead of creating a new object. `clone` avoids that bug making a
- * shallow copy of the objects along the update path
- * so no object is mutated in place.
- *
- * Before changing this function, please read through the following
- * discussions.
- *
- * @see https://github.com/developit/linkstate
- * @see https://github.com/jaredpalmer/formik/pull/123
- */
+// inspired by https://github.com/formium/formik/blob/master/packages/formik/src/utils.ts
+// better tailored for change detection
 export function setIn(obj, path, value) {
-    let res = clone(obj); // this keeps inheritance when obj is a class
+    let res = shallowCopy(obj);
     let resVal = res;
     let i = 0;
     let pathArray = toPath(path);
-
     for (; i < pathArray.length - 1; i++) {
         const currentPath = pathArray[i];
         let currentObj = getIn(obj, pathArray.slice(0, i + 1));
-
-        if (currentObj && (isObj(currentObj) || Array.isArray(currentObj))) {
-            resVal = resVal[currentPath] = clone(currentObj);
+        if (currentObj && (isObj(currentObj) || isArray(currentObj))) {
+            resVal = resVal[currentPath] = shallowCopy(currentObj);
         } else {
             const nextPath = pathArray[i + 1];
             resVal = resVal[currentPath] = isInteger(nextPath) && Number(nextPath) >= 0 ? [] : {};
         }
     }
-
     // Return original object if new value is the same as current
-    if ((i === 0 ? obj : resVal)[pathArray[i]] === value) {
-        return obj;
-    }
-
-    if (value === undefined) {
-        delete resVal[pathArray[i]];
-    } else {
-        resVal[pathArray[i]] = value;
-    }
-
+    if ((i === 0 ? obj : resVal)[pathArray[i]] === value) return obj;
+    if (value === undefined) delete resVal[pathArray[i]];
+    else resVal[pathArray[i]] = value;
     // If the path array has a single element, the loop did not run.
     // Deleting on `resVal` had no effect in this scenario, so we delete on the result instead.
-    if (i === 0 && value === undefined) {
-        delete res[pathArray[i]];
-    }
-
+    if (i === 0 && value === undefined) delete res[pathArray[i]];
     return res;
 }
 
 
-export const selectAndOthers = ({props, select, fallbacks}) => {
+export const selectAndOthers = ({props, select, fallbacks} = {}) => {
     let selected = {};
     let others = {...props};
     for (let i = 0; i < select.length; i++) {
@@ -680,6 +623,7 @@ export const getItemsThatExistInBothArrays = (arr1, arr2, {findById} = {}) => {
     return [...arr1].filter(it => isInArray(arr2, it));
 };
 
+// const union = (arr, ...args) => [...new Set(arr.concat(...args))]
 //union
 export const combineArraysAndDeduplicate = (arr1, arr2, {findById} = {}) => {
     return [...arr1, ...excludeItemsFromAray(arr2, arr1, {findById})];
